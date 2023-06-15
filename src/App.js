1f6c89408c0705cfdd7ef06e7ebea1f0a1ba58ca
Abuk974 <120749240+Abuk974@users.noreply.github.com>
2023-06-15 09:40:45 +0300

    Update app.js
    open-source

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <small>
+          <a
+            href="https://github.com/Abuk974/fineapp.git"
+            target="-blank"
+            >Open-source code</a
+          > 
+         Coded by  Abuk Sedonia 
+        </small>
     </div>
   );
 }
